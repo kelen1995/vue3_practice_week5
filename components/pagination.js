03af2a5d22baab3fa387d1119f65d39355539e5d
kyle <sdfcvbjkl@gmail.com>
2022-02-28 22:36:33 +0800

    fix: 修正上/下一頁禁用後還可以送出請求的問題

--- components/pagination.js
+++ components/pagination.js
@@ -3,14 +3,14 @@ export default {
     template: `
     <nav aria-label="Page navigation example">
         <ul class="pagination justify-content-center">
-        <li class="page-item" :class="{disabled:!pagination.has_pre}" @click="switchPage(pagination.current_page-1)">
-            <a class="page-link" href="#" aria-label="Previous">
+        <li class="page-item" :class="{disabled:!pagination.has_pre}">
+            <a class="page-link" href="#" aria-label="Previous" @click="switchPage(pagination.current_page-1)">
                 <span aria-hidden="true">&laquo;</span>
             </a>
         </li>
         <li class="page-item" v-for="page in pagination.total_pages"  @click="switchPage(page)"><a class="page-link" href="#">{{page}}</a></li>
-        <li class="page-item" :class="{disabled:!pagination.has_next}" @click="switchPage(pagination.current_page+1)">
-            <a class="page-link" href="#" aria-label="Next">
+        <li class="page-item" :class="{disabled:!pagination.has_next}">
+            <a class="page-link" href="#" aria-label="Next" @click="switchPage(pagination.current_page+1)">
                 <span aria-hidden="true">&raquo;</span>
             </a>
         </li>
