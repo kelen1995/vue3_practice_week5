a553b52b8e8f6e4dc57679ad65010d592c633ef4
kyle <sdfcvbjkl@gmail.com>
2022-02-28 22:29:47 +0800

    feat: 調整分頁元件外觀
    - 位置改成畫面中央
    - 上一頁、下一頁改為箭頭提示

--- components/pagination.js
+++ components/pagination.js
@@ -2,10 +2,18 @@
 export default {
     template: `
     <nav aria-label="Page navigation example">
-        <ul class="pagination">
-        <li class="page-item" :class="{disabled:!pagination.has_pre}" @click="switchPage(pagination.current_page-1)"><a class="page-link" href="#">Previous</a></li>
+        <ul class="pagination justify-content-center">
+        <li class="page-item" :class="{disabled:!pagination.has_pre}" @click="switchPage(pagination.current_page-1)">
+            <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+            </a>
+        </li>
         <li class="page-item" v-for="page in pagination.total_pages"  @click="switchPage(page)"><a class="page-link" href="#">{{page}}</a></li>
-        <li class="page-item" :class="{disabled:!pagination.has_next}" @click="switchPage(pagination.current_page+1)"><a class="page-link" href="#">Next</a></li>
+        <li class="page-item" :class="{disabled:!pagination.has_next}" @click="switchPage(pagination.current_page+1)">
+            <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+            </a>
+        </li>
         </ul>
     </nav>`,
     props:['pagination'],
